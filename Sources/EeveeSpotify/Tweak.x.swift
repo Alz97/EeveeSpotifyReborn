26a9369e71e502441e756b5f5ad1df0e55f7a547
WADBB <kiki8.fs@gmail.com>
2026-01-16 18:39:51 +0100

    Add support for Spotify 9.1.x versions

--- Sources/EeveeSpotify/Tweak.x.swift
+++ Sources/EeveeSpotify/Tweak.x.swift
@@ -14,6 +14,7 @@ struct BasePremiumPatchingGroup: HookGroup { }
 struct IOS14PremiumPatchingGroup: HookGroup { }
 struct NonIOS14PremiumPatchingGroup: HookGroup { }
 struct IOS14And15PremiumPatchingGroup: HookGroup { }
+struct V91PremiumPatchingGroup: HookGroup { } // For Spotify 9.1.x versions
 struct LatestPremiumPatchingGroup: HookGroup { }
 
 func activatePremiumPatchingGroup() {
@@ -21,6 +22,11 @@ func activatePremiumPatchingGroup() {
     
     if EeveeSpotify.hookTarget == .lastAvailableiOS14 {
         IOS14PremiumPatchingGroup().activate()
+    }
+    else if EeveeSpotify.hookTarget == .v91 {
+        // 9.1.x versions: Use NonIOS14 hooks but skip offline content hooks
+        NonIOS14PremiumPatchingGroup().activate()
+        V91PremiumPatchingGroup().activate()
     }
     else {
         NonIOS14PremiumPatchingGroup().activate()
@@ -35,7 +41,7 @@ func activatePremiumPatchingGroup() {
 }
 
 struct EeveeSpotify: Tweak {
-    static let version = "6.2.2"
+    static let version = "7.0.0"
     
     static var hookTarget: VersionHookTarget {
         let version = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
@@ -45,12 +51,35 @@ struct EeveeSpotify: Tweak {
             return .lastAvailableiOS15
         case "8.9.8":
             return .lastAvailableiOS14
+        case "9.1.0", "9.1.6", "9.1.12":
+            // 9.1.x versions don't have offline content helper classes
+            return .v91
         default:
             return .latest
         }
     }
     
     init() {
+        // For 9.1.x versions, handle differently
+        if EeveeSpotify.hookTarget == .v91 {
+            // Premium patching
+            if UserDefaults.patchType.isPatching {
+                BasePremiumPatchingGroup().activate()
+            }
+            
+            // Lyrics for 9.1.x (experimental)
+            if UserDefaults.lyricsSource.isReplacingLyrics {
+                BaseLyricsGroup().activate()
+                V91LyricsGroup().activate() // V91-specific lyrics group
+            }
+            
+            // Settings integration
+            UniversalSettingsIntegrationGroup().activate()
+            
+            return // Exit early for 9.1.x versions
+        }
+        
+        // Original logic for all other versions
         if UserDefaults.experimentsOptions.showInstagramDestination {
             InstgramDestinationGroup().activate()
         }
@@ -66,6 +95,9 @@ struct EeveeSpotify: Tweak {
         if UserDefaults.lyricsSource.isReplacingLyrics {
             BaseLyricsGroup().activate()
             
+            // Activate error handling hooks (not compatible with 9.1.x)
+            LyricsErrorHandlingGroup().activate()
+            
             if EeveeSpotify.hookTarget == .latest {
                 ModernLyricsGroup().activate()
             }
@@ -73,5 +105,9 @@ struct EeveeSpotify: Tweak {
                 LegacyLyricsGroup().activate()
             }
         }
+        
+        // Settings integration for non-9.1.x versions
+        UniversalSettingsIntegrationGroup().activate()
+        SettingsIntegrationGroup().activate()
     }
 }
